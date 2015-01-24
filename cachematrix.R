cd71e275957c5b4e2ed3c2f995b8798c6f76fab2
Víctor <victor.martin.gairal@gmail.com>
2015-01-24 23:28:52 +0100

    Solution to assesment 2

--- cachematrix.R
+++ cachematrix.R
@@ -2,15 +2,38 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## makeCacheMatrix: Given a matrix x, stores the value in the environment
+## and resets m value. Creates setinv and getinv functions accessed when
+## solving the matrix.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    m <- NULL
+    set <- function(y) {
+        x <<- y
+        m <<- NULL
+    }
+    get <- function() x
+    setinv <- function(solve) m <<- solve
+    getinv <- function() m
+    list(set = set, get = get,
+         setinv = setinv,
+         getinv = getinv)
 }
 
 
-## Write a short comment describing this function
+## cacheSolve: Given a cacheMatrix function checks m value of the enviornment.
+## If m does not exist --> calculates it using solve and stores it via
+## setinv function.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+    m <- x$getinv()
+    if(!is.null(m)) {
+        message("getting cached data")
+        return(m)
+    }
+    data <- x$get()
+    m <- solve(data, ...)
+    x$setinv(m)
+    m
 }
